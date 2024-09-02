2a5aa2cf9d277fae555524bed274085284970619
NathanGab <151534404+NathanGab@users.noreply.github.com>
2024-09-02 17:44:02 -0300

    Programa.java

---
+++ atividade3/Programa.java
@@ -0,0 +1,12 @@
+package Exercicio7;
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa("Carlos");
+        pessoa.acordar();
+        pessoa.andar();
+        pessoa.comer();
+        pessoa.dormir();
+    }
+}
