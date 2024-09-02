b000e964e626cb47b5518aa791124d00c8659103
NathanGab <151534404+NathanGab@users.noreply.github.com>
2024-09-02 16:50:19 -0300

    IMC.java

---
+++ atividade1/IMC.java
@@ -0,0 +1,17 @@
+public class IMC {
+    public double calcularIMC(double peso, double altura) {
+        return peso / (altura * altura);
+    }
+
+    public String interpretarIMC(double imc) {
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc >= 18.5 && imc < 24.9) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 29.9) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidade";
+        }
+    }
+}
